62bda640c7e24e5c52d6c81611a232de8fb05979
BlackJack <km576843117@hotmail.com>
2018-03-12 23:01:42 +0800

    home page api except send

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,27 +30,33 @@ class HomeController extends controller
      * 获取首页用户信息
      */
     public function userInfo(){
-
+        $token = $request->session()->get("token");
+        $this->weiboClient= new \SaeTClientV2( env('WB_AKEY') , env('WB_SKEY') , $token['access_token'] );
+        return $this->weiboClient->show_user_by_id($token['uid']);
     }
 
     /*
      * 获取热门话题
      */
     public function topic(){
-
+        $token = $request->session()->get("token");
+        $this->weiboClient= new \SaeTClientV2( env('WB_AKEY') , env('WB_SKEY') , $token['access_token'] );
+        return $this->weiboClient->daily_trends();
     }
 
     /*
      * 发送一条微博
      */
     public function sendLun(){
-
+        
     }
 
     /*
      * 感兴趣的用户列表
      */
     public function interestingPeople(){
-
+        $token = $request->session()->get("token");
+        $this->weiboClient= new \SaeTClientV2( env('WB_AKEY') , env('WB_SKEY') , $token['access_token'] );
+        return $this->weiboClient->suggestions_may_interested();
     }
 }
